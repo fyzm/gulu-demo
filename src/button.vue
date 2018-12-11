bbc1b31dc063537c133ecf224275b2d35e0a07ae
fyzm <fangyuan931018@gmail.com>
2018-12-11 21:06:23 +0800

    检测子元素是否规范

--- src/button.vue
+++ src/button.vue
@@ -34,38 +34,19 @@
         0% {transform: rotate(0deg);}
         100% {transform: rotate(360deg);}
     }
-    .g-button{
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+    .g-button{ font-size: var(--font-size); height: var(--button-height);padding: 0 1em;
+        border-radius: var(--border-radius);border: 1px solid var(--border-color);background: var(--button-bg);
         display: inline-flex;justify-content: center;align-items: center;
         vertical-align: middle;
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-        &:focus {
-            outline: none;
-        }
-        > .icon{
-            order: 1;margin-right: .1em;
-        }
-        > .content{
-            order: 2;
-        }
+        &:hover { border-color: var(--border-color-hover);}
+        &:active { background-color: var(--button-active-bg);}
+        &:focus { outline: none;}
+        > .icon{ order: 1;margin-right: .1em;}
+        > .content{order: 2;}
         &.icon-right{
-            > .icon{
-                order: 2;margin-left: .1em;margin-right: 0;
-            }
+            > .icon{ order: 2;margin-left: .1em;margin-right: 0;}
             > .content {order: 1}
         }
-        .loading{
-            animation: spin  1s infinite linear;
-        }
+        .loading{ animation: spin  1s infinite linear;}
     }
 </style>
